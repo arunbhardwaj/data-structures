9690dab68cf885cc3acbb769eced51f291dc369c
Arun Bhardwaj <arun.b.95@gmail.com>
2021-09-22 18:39:11 -0400

    Finished functional q and stack.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,37 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
+  var enqPosition = 0;
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    storage[enqPosition] = value;
+    length++; enqPosition++;
   };
 
   someInstance.dequeue = function() {
+    if (length <= 0) {
+      return null;
+    }
+
+    // Doesn't guarantee either but tends to on most browsers
+    var firstKey = Object.keys(storage)[0];
+    var firstValue = storage[firstKey];
+
+    // Doesn't guarntee the first key/value
+    // for (var key in storage) {
+    //   var firstKey = key;
+    //   var firstValue = storage[key];
+    //   break;
+    // }
+    delete storage[firstKey]; // isn't executing
+    length--;
+    return firstValue;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,28 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.pop = function() {
+    if (length <= 0) {
+      return null;
+    }
+
+    // Doesn't guarantee either but tends to on most browsers
+    var lastValue = storage[length - 1];
+    delete storage[length - 1];
+    length--;
+    return lastValue;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
